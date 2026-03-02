144e202246f6b2c548ad9da9a010a0bbbfdffb15
Sarveshwar Hirve <sarveshwar@gmail.com>
2026-03-02 13:40:28 +0530

    day3 array practice

--- practise.js
+++ practise.js
@@ -1,5 +1,7 @@
 let tasks = ["study", "gym", "eat"];
 
+tasks.push('sleep')
+
 console.log(tasks);        // whole list
 console.log(tasks[0]);     // first item
 console.log(tasks.length); // number of items
